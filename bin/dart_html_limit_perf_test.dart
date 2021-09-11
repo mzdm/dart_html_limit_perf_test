6438f1c450476f6104fb5c41f3696b2ba65b84d8
Matej Zidek <zidek.matt@gmail.com>
2021-09-11 13:12:21 +0200

    add bench comparison

--- bin/dart_html_limit_perf_test.dart
+++ bin/dart_html_limit_perf_test.dart
@@ -3,6 +3,18 @@ import 'package:html/dom.dart';
 import 'package:html/parser.dart' show parse;
 import 'package:http/http.dart' as http;
 
+class BenchHtmlLimit extends BenchmarkBase {
+  BenchHtmlLimit(this.document) : super('BenchHtmlLimit');
+
+  final Document document;
+
+  @override
+  void run() {
+    document.querySelectorAll('.mw-headline', limit: 4);
+    // print(elements.map((e) => e.text).toList());
+  }
+}
+
 class BenchHtml extends BenchmarkBase {
   BenchHtml(this.document) : super('BenchHtml');
 
@@ -10,19 +22,21 @@ class BenchHtml extends BenchmarkBase {
 
   @override
   void run() {
-    // final elements = document.querySelectorAll('.mw-headline');
-    final elements = document.querySelectorAll('.mw-headline', limit: 4);
-
-    print(elements.map((e) => e.text).toList());
+    document.querySelectorAll('.mw-headline').take(4).toList();
+    // print(elements.map((e) => e.text).toList());
   }
 }
+
 
 void main() async {
   var url = Uri.parse('https://en.wikipedia.org/wiki/Earth');
   var response = await http.get(url);
   if (response.statusCode == 200) {
     final document = parse(response.body);
-    BenchHtml(document).report();
+    final benchmarks = [BenchHtml(document), BenchHtmlLimit(document)];
+    for (var benchmark in benchmarks) {
+      benchmark.report();
+    }
   } else {
     throw ('Invalid response status');
   }
